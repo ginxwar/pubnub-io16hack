69009cea317def15273be8f36c8ea9396c888076
Kevin Wu <kevin.wu@gmail.com>
2016-05-20 06:16:30 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var secrets = require('./secrets')
+
+var pubnub = require("pubnub")({
+    ssl           : true,  // <- enable TLS Tunneling over TCP
+    publish_key   : secrets.PUB_KEY,
+    subscribe_key : secrets.SUB_KEY
+})
+
+var http = require('http')
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+
+app.use(bodyParser.json())
+
+
+// var message = { "patrick" : "dirtbag" }
+
+
+
+app.post('/', function(req, res, next) {
+  
+  pubnub.publish({ 
+    channel   : 'amazonEcho',
+    message   : req.body,
+    callback  : function(e) { console.log( "SUCCESS!", e ); },
+    error     : function(e) { console.log( "FAILED! RETRY PUBLISH!", e ); }
+  })
+  
+  // things are always good
+  res.send(200)
+  return next()
+})
+
+
+
+app.listen(3000)
---
+++ secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+  PUB_KEY:'pub-c-9913cf0c-6432-4c99-8fd5-0eafa6af84cc',
+  SUB_KEY:'sub-c-0d1c27a6-1e44-11e6-b700-0619f8945a4f'
+}
